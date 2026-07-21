1666559c0ff3b44c0a7620371d0feb418df72b71
Bakir Gracic <bakirgdev@gmail.com>
2026-07-21 23:15:40 +0200

    init multiplatform xcode proj

---
+++ CrocApp/CrocApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CrocApp
+//
+//  Created by admin on 21.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CrocApp/CrocApp/CrocAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrocAppApp.swift
+//  CrocApp
+//
+//  Created by admin on 21.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct CrocAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
